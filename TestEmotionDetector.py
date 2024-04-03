c200dc1f86c706fc1585add73965a404b26ec495
amr esmail <120759715+amrEsma3il@users.noreply.github.com>
2024-04-03 02:51:25 +0200

    Update TestEmotionDetector.py

--- TestEmotionDetector.py
+++ TestEmotionDetector.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 from flask import Flask,request,jsonify
-import os
-from werkzeug.utils import secure_filename
 from PIL import Image
 from keras.preprocessing import image
 
@@ -43,41 +41,41 @@ emotionSeries=[]
 
 
 
-def video_prediction_emotion(file_path):
+# def video_prediction_emotion(file_path):
      
 
-     cap = cv2.VideoCapture(file_path)
+#      cap = cv2.VideoCapture(file_path)
 
-     if os.path.isfile(file_path):
-       os.remove(file_path)
+#      if os.path.isfile(file_path):
+#        os.remove(file_path)
 
-     while True:
-    # Find haar cascade to draw bounding box around face
-       ret, frame = cap.read()
-       frame = cv2.resize(frame, (1280, 720))
-       if not ret:
-        break
-       face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-       gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # detect faces available on camera
-       num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
-
-    # take each face available on the camera and Preprocess it
-       for (x, y, w, h) in num_faces:
-        # cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-
-        # predict the emotions
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        # cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-       emotionSeries.append(emotion_dict[maxindex])
-#     cv2.imshow('Emotion Detection', frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#      while True:
+#     # Find haar cascade to draw bounding box around face
+#        ret, frame = cap.read()
+#        frame = cv2.resize(frame, (1280, 720))
+#        if not ret:
 #         break
-     return jsonify(emotionSeries)
+#        face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+#        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#     # detect faces available on camera
+#        num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+#     # take each face available on the camera and Preprocess it
+#        for (x, y, w, h) in num_faces:
+#         # cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+#         roi_gray_frame = gray_frame[y:y + h, x:x + w]
+#         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+
+#         # predict the emotions
+#         emotion_prediction = emotion_model.predict(cropped_img)
+#         maxindex = int(np.argmax(emotion_prediction))
+#         # cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+#        emotionSeries.append(emotion_dict[maxindex])
+# #     cv2.imshow('Emotion Detection', frame)
+# #     if cv2.waitKey(1) & 0xFF == ord('q'):
+# #         break
+#      return jsonify(emotionSeries)
 
 
 
@@ -97,19 +95,13 @@ def video_prediction_emotion(file_path):
 def image_predict_emotion(file_path):
   
 
-  
-     img = cv2.imread(file_path)
+   img = Image.open(file_path)
 
-     #مسح مسار الصورة
-# If file exists, delete it.
+     # nparr = np.fromstring(file_path.read(), np.uint8)
+     # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
 
-
-     if os.path.isfile(file_path):
-       os.remove(file_path)
-
-
-
-     img = cv2.resize(img, (1280, 720))
+     img = cv2.resize(np.array(img), (1280, 720))
 
      
      face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
@@ -179,14 +171,10 @@ def insert():
   
      f=request.files.get('file')
 
-     basepath=os.path.dirname(__file__)
-     file_path=os.path.join(basepath,'upload',secure_filename(f.filename))
-     f.save(file_path)
 
      if (str(file_path).split("."))[-1].lower() in ['png','jpg','svg','igmp','JPEG']:
-       return image_predict_emotion(file_path=file_path)
-     else:
-       return video_prediction_emotion(file_path=file_path)
+       return image_predict_emotion(file_path=f)
+     
 
 
   
