76bf9ad7adc833d2547561f06831fa873a00b0c8
amr esmail <120759715+amrEsma3il@users.noreply.github.com>
2024-04-05 17:29:55 +0200

    Update TestEmotionDetector.py

--- TestEmotionDetector.py
+++ TestEmotionDetector.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 from flask import Flask,request,jsonify
+
 from PIL import Image
 from keras.preprocessing import image
 
@@ -14,17 +15,6 @@ from keras.preprocessing import image
 
 
 
-# emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-# # load json and create model
-# json_file = open('model/emotion_model.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# emotion_model = model_from_json(loaded_model_json)
-
-# # load weights into new model
-# emotion_model.load_weights("model/emotion_model.h5")
-# emotionSeries=[]
 
 
 # start the webcam feed
@@ -86,64 +76,82 @@ from keras.preprocessing import image
 
 
 
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load json and create model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+emotionSeries=[]
 
 
 
 
 
 
-# def image_predict_emotion(file_path):
+def image_predict_emotion(file_path):
   
 
-#    img = Image.open(file_path)
+  
+     
 
-#      # nparr = np.fromstring(file_path.read(), np.uint8)
-#      # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+     #مسح مسار الصورة
+# If file exists, delete it.
+
+
+     img = Image.open(file_path)
+
+     # nparr = np.fromstring(file_path.read(), np.uint8)
+     # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     
 
-#      img = cv2.resize(np.array(img), (1280, 720))
+     img = cv2.resize(np.array(img), (1280, 720))
 
      
-#      face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-#      gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+     face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#     # detect faces available on camera
-#      num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    # detect faces available on camera
+     num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
     
 
-#     # take each face available on the camera and Preprocess it
-#      for (x, y, w, h) in num_faces:
-#             #  cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 5)
+    # take each face available on the camera and Preprocess it
+     for (x, y, w, h) in num_faces:
+            #  cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 5)
 
-#         # الاربع سطور دوول مسوولين عن تحديد الرقم من الديكشناري
-#         #وبالتالي تحديد اسم الكلاس
+        # الاربع سطور دوول مسوولين عن تحديد الرقم من الديكشناري
+        #وبالتالي تحديد اسم الكلاس
 
-#              roi_gray_frame = gray_frame[y:y + h, x:x + w]
-#              cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+             roi_gray_frame = gray_frame[y:y + h, x:x + w]
+             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
 
-#         # predict the emotions
-#              emotion_prediction = emotion_model.predict(cropped_img)      #important هاااااااااااااااااااااام
-#              maxindex = int(np.argmax(emotion_prediction))
+        # predict the emotions
+             emotion_prediction = emotion_model.predict(cropped_img)      #important هاااااااااااااااااااااام
+             maxindex = int(np.argmax(emotion_prediction))
 
-#             #  emotionSeries.append( emotion_dict[maxindex])
+            #  emotionSeries.append( emotion_dict[maxindex])
 
 
-#     #          cv2.putText(img, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    #          cv2.putText(img, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-#     #  cv2.imshow('Emotion Detection', img)
+    #  cv2.imshow('Emotion Detection', img)
 
-#     #  cv2.waitKey(0)
+    #  cv2.waitKey(0)
 
    
 
 
 
-#     #  img.release()
-#     #  cv2.destroyAllWindows()
+    #  img.release()
+    #  cv2.destroyAllWindows()
     
 
-#      return f"{emotion_dict[maxindex]}"
+     return f"{emotion_dict[maxindex]}"
 
    
 
@@ -161,33 +169,13 @@ app=Flask(__name__)
 
   
 
-@app.route('/predictttttt',methods=["POST","GET"])
+@app.route('/predict',methods=["POST","GET"])
 
 def insert():
- return {"msg":"hello"}
- # if request.method=="POST":
-
- #  if 'file' in request.files:
-  
- #     f=request.files.get('file')
-
-
- #     if (str(file_path).split("."))[-1].lower() in ['png','jpg','svg','igmp','JPEG']:
- #       return image_predict_emotion(file_path=f)
-     
-
-
-  
-     
-
-
-
-
+ 
+   return image_predict_emotion(file_path='data/test/happy/PrivateTest_1140198.jpg')
    
 
-
-
-   
 
 
 
